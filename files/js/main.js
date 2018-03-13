d9ab7e31a7fa927cd6ebd165198a294bca4ad98a
brandon <bparkerproductions@gmail.com>
2018-03-12 21:27:38 -0800

    jquery starter

---
+++ files/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	Page.init();
+});
+
+//main app/page functions
+var Page = {
+	init: function(){
+		View.init();
+	},
+
+	setListeners(){
+		//$("body").on('load', console.log('ey'));
+	}
+}
+
+//presentation functions
+var View = {
+	init: function(){
+		
+	}
+}

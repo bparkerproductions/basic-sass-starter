1795a632c1039272ca1383c5470346368dc57c7e
brandon <bparkerproductions@gmail.com>
2018-09-12 23:12:28 -0800

    removed js, replace jq with vue

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-$(document).ready(function(){
-	Page.init();
-});
-
-//main app/page functions
-var Page = {
-	init: function(){
-		View.init();
-	},
-
-	setListeners(){
-		//$("body").on('load', console.log('ey'));
-	}
-}
-
-//presentation functions
-var View = {
-	init: function(){
-		
-	}
-}
+/*
+ * Main app JS
+ */

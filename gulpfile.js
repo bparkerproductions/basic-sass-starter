4598bc5da0d61ca5a62bd62e33a79fe43c3bdcc1
Brandon <bparkerproductions@gmail.com>
2020-06-05 13:51:42 -0700

    fix mixin errors

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,9 @@
 var gulp = require('gulp');
-var autoprefexer = require('gulp-autoprefixer');
 var sass = require('gulp-sass');
-
-//prefixes
-gulp.task('prefix', function(){
-	gulp.src('build/main.css')
-	.pipe(autoprefexer({
-		overrideBrowserslist: ['last 2 versions']
-	}))
-	.pipe(gulp.dest('build'))
-});
 
 //compile scss
 gulp.task('sass', function() {
-  return gulp.src("css/scss/global/main.scss")
+  return gulp.src("css/scss/main.scss")
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('build'))
 });
